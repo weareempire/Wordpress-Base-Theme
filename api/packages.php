eba97dadb797e55ffe3fd63af1b2dabe096cd5c2
Lee Fuller <lee@weareempire.co.uk>
2015-09-08 16:11:41 +0100

    reset

--- api/packages.php
+++ api/packages.php
@@ -1,36 +1,21 @@
 <?php
-
-  $version = '1.0.002'; // format 0.0.000
-
-  $date = '2015-09-08'; // format YYYY-MM-DD
-
-  $theme = 'empire_base';
-
-  $file = $theme . '.zip';
-
-  $directory = 'https://github.com/weareempire/Wordpress-Base-Theme/tree/master/api/download.php';
-
-
-  $packages[ $theme ] = array( //Replace theme with theme stylesheet slug that the update is for
-
+// Theme with update info
+$packages['empire_base'] = array( //Replace theme with theme stylesheet slug that the update is for
     'versions' => array(
-
-      $version => array( // Array name should be set to current version of update
-
-        'version' => $version, // Current version available
-        'date' => $date, // Date version was released
-        'package' => $directory . '?key=' . md5( $file . mktime( 0,0,0, date( 'n' ), date( 'j' ), date( 'Y' ) ) ),
-        'file_name' => $file,	// File name of theme zip file
-        'author' => 'We Are Empire', // Author of theme
-        'name' => 'We Are Empire', // Name of theme
-        'requires' => '4.3', // Wordpress version required
-        'tested' => '4.3', // WordPress version tested up to
-      )
-
+        '1.0.002' => array( //Array name should be set to current version of update
+            'version' => '1.0.002', //Current version available
+            'date' => '2015-09-08', //Date version was released
+            //theme.zip is the same as file_name
+            'package' => 'http://demo/youareempire.co.uk/theme/api/download.php?key=' . md5('empire_base.zip' . mktime(0,0,0,date("n"),date("j"),date("Y"))),
+            //file_name is the name of the file in the update folder.
+            'file_name' => 'empire_base.zip',	//File name of theme zip file
+            'author' => 'We Are Empire', //Author of theme
+            'name' => 'We Are Empire', //Name of theme
+            'requires' => '4.3', //Wordpress version required
+            'tested' => '4.3', //WordPress version tested up to
+        )
     ),
-
     'info' => array(
-        'url' => 'http://weareempire.co.uk'
+        'url' => 'http://url_to_your_theme_site'  // Website devoted to theme if available
     )
-
-  );
+);
